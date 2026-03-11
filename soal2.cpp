6b095b7087d71d504e7f30614dd7ec08aa8a3903
ikankeselek <154447514+ikankeselek@users.noreply.github.com>
2026-03-12 00:10:39 +0700

    Update soal2.cpp

--- soal2.cpp
+++ soal2.cpp
@@ -1,6 +1,25 @@
-// Soal ini dijawab menggunakan bahasa C, tapi tidak apa-apa jika menggunakan extension CPP
+#include <stdio.h>
+#include <conio.h>
 
-/*
-Buat program yang membaca satu karakter menggunakan getchar(), getche(), dan getch(). 
-Tampilkan karakter yang dimasukkan setelah setiap input.
-*/
+int main() {
+    char c;
+
+    printf("Input karakter dengan getchar(): ");
+    c = getchar();
+    printf("Karakter yang dimasukkan: %c\n\n", c);
+
+    printf("Input karakter dengan getche(): ");
+    c = getche();
+    printf("\nKarakter yang dimasukkan: %c\n\n", c);
+
+    printf("Input karakter dengan getch(): ");
+    c = getch();
+    printf("\nKarakter yang dimasukkan: %c\n", c);
+
+}
+
+// Soal Ini dikerjakan oleh Aditya dan Gabrielle
+// Getchar, Berbeda dengan scanf dimana scanf bisa mengambil banyak karakter, getchar hanya bisa menampung 1 karakter. Getchar juga memerlukan user menekan tombol enter untuk 
+// Getche Mirip dengan Getchar, tetapi perbedaannya adalah getche tidak memerlukan user menekan tombol enter
+// Getch merupakan suatu varian dari getche. sama sama tidak perlu menekan tombol enter, tetapi perbedaannya adalah getch merahasiakan nilai yg dibacanya, tidak muncul di output
+
